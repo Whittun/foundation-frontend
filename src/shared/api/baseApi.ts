6470915e1563fed57fb8a3d036da9356532cdbea
Aleksandr <whittun@gmail.com>
2026-06-19 22:23:46 +0300

    build: add env

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const baseApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3000/',
+    baseUrl: import.meta.env.VITE_API_URL,
     credentials: 'include',
   }),
   tagTypes: ['Auth', 'Habits', 'HabitLevels', 'YearRatings', 'Objectives'],
