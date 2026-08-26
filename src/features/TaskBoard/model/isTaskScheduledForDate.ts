8050c6cb7193277c33e28281dc5e299b74fc6b7c
Aleksandr <whittun@gmail.com>
2026-08-26 12:36:10 +0300

    feat: add logic for processing weekly intervals

--- src/features/TaskBoard/model/isTaskScheduledForDate.ts
+++ src/features/TaskBoard/model/isTaskScheduledForDate.ts
@@ -1,20 +1,50 @@
 import type { Task } from './types';
 
-export const isTaskScheduledForDate = (task: Task, date: string): boolean => {
-  if (task.schedule.type !== 'daily') {
+const getWeekStart = (date: Date) => {
+  const day = date.getUTCDay();
+  const daysFromMonday = (day + 6) % 7;
+
+  const weekStart = new Date(date);
+
+  weekStart.setUTCDate(weekStart.getUTCDate() - daysFromMonday);
+
+  return weekStart;
+};
+
+export const isTaskScheduledForDate = (task: Task, targetDate: string): boolean => {
+  if (targetDate < task.startDate) {
     return false;
   }
 
-  if (date < task.startDate) {
-    return false;
+  if (task.schedule.type === 'daily') {
+    const start = new Date(task.startDate);
+    const target = new Date(targetDate);
+
+    const differenceInMs = target.getTime() - start.getTime();
+    const differenceInDays = differenceInMs / (1000 * 60 * 60 * 24);
+
+    return differenceInDays % task.schedule.every === 0;
   }
 
-  const start = new Date(task.startDate);
-  const target = new Date(date);
+  if (task.schedule.type === 'weekly') {
+    const target = new Date(targetDate);
+    const weekday = target.getUTCDay();
 
-  const differenceInMs = target.getTime() - start.getTime();
+    if (!task.schedule.days.includes(weekday)) {
+      return false;
+    }
 
-  const differenceInDays = differenceInMs / (1000 * 60 * 60 * 24);
+    const start = new Date(task.startDate);
 
-  return differenceInDays % task.schedule.every === 0;
+    const startWeek = getWeekStart(start);
+    const targetWeek = getWeekStart(target);
+
+    const differenceInMs = targetWeek.getTime() - startWeek.getTime();
+
+    const differenceInWeeks = differenceInMs / (1000 * 60 * 60 * 24 * 7);
+
+    return differenceInWeeks % task.schedule.every === 0;
+  }
+
+  return false;
 };
